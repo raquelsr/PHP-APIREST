e1f9124625c8f94688023916a5b8cfd5ec34d5eb
Raquelsr <raquelserranor@hotmail.com>
2017-11-16 20:28:07 +0100

    EntityResult - DELETE , PUT y OPTIONS

--- src/AppBundle/Controller/ApiResultController.php
+++ src/AppBundle/Controller/ApiResultController.php
@@ -107,7 +107,7 @@ class ApiResultController extends Controller
             return new JsonResponse(
                 new Message(
                     Response::HTTP_NOT_FOUND,
-                    'Usuario no encontrado'
+                    'El usuario no existe'
                 ),
                 Response::HTTP_NOT_FOUND
             );
@@ -125,5 +125,118 @@ class ApiResultController extends Controller
         return new JsonResponse($result, Response::HTTP_CREATED);
     }
 
+    /**
+     *
+     * @param int $resultId Result id
+     *
+     * @return Response
+     *
+     * @Route("/{resultId}", name="miw_delete_results", requirements={"resultId": "\d+"})
+     * @Method(Request::METHOD_DELETE)
+     */
+    public function deleteResultAction(int $resultId)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $result = $entityManager->getRepository(Result::class)->find($resultId);
+
+        if (empty($result)) {   // 404 - Not Found
+            return new JsonResponse(
+                new Message(
+                    Response::HTTP_NOT_FOUND,
+                    Response::$statusTexts[404]
+                ),
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        $entityManager->remove($result);
+        $entityManager->flush();
+
+        return new Response(null, Response::HTTP_NO_CONTENT);
+    }
+
+
+    /**
+     *
+     * @param Request $request request
+     * @param int     $resultId  Result id
+     *
+     * @return JsonResponse
+     *
+     * @Route("/{resultId}", name="miw_put_results", requirements={"resultId": "\d+"})
+     * @Method(Request::METHOD_PUT)
+     */
+    public function putResultAction(Request $request, int $resultId)
+    {
+        $body = $request->getContent(false);
+        $postData = json_decode($body, true);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        /** @var Result $result */
+        $result = $entityManager->getRepository(Result::class)->find($resultId);
+
+        if (empty($result)) {    // 404 - Not Found
+            return new JsonResponse(
+                new Message(Response::HTTP_NOT_FOUND, Response::$statusTexts[404]),
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        if (isset($postData['result'])) {
+            $result->setResult($postData['result']);
+        }
+
+        if (isset($postData['user'])) {
+            $userReposity = $entityManager->getRepository(User::class);
+            /** @var User $user */
+            $user = $userReposity->find($postData['user']);
+            if (empty($user)){
+                return new JsonResponse(
+                    new Message(
+                        Response::HTTP_NOT_FOUND,
+                        'El usuario no existe'
+                    ),
+                    Response::HTTP_NOT_FOUND
+                );
+            } else {
+                $result->setUserId($user);
+            }
+        }
+
+        $result->setTime(new \DateTime('now'));
+
+        $entityManager->merge($result);
+        $entityManager->flush();
+
+        return new JsonResponse($result, 209);    // 209 - Content Returned
+    }
+
+
+    /**
+     * @param int $resultId Result id
+     *
+     * @return JsonResponse
+     *
+     * @Route(
+     *     "/{resultId}",
+     *     name = "miw_options_results",
+     *     defaults = {"resultId" = 0},
+     *     requirements = {"resultId": "\d+"}
+     *     )
+     * @Method(Request::METHOD_OPTIONS)
+     */
+    public function optionsResultAction(int $resultId)
+    {
+        $methods = ($resultId)
+            ? ['GET', 'PUT', 'DELETE']
+            : ['GET', 'POST'];
+
+        return new JsonResponse(
+            null,
+            Response::HTTP_OK,
+            ['Allow' => implode(', ', $methods)]
+        );
+    }
 
 }
